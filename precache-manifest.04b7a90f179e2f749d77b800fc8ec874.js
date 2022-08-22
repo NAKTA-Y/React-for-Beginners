032963bdf57d95f632dae59387090cb5ca455688
nakta <nakta0511@gmail.com>
2022-08-22 21:51:52 +0900

    Updates

---
+++ precache-manifest.04b7a90f179e2f749d77b800fc8ec874.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2fb414a9cc68004936d",
+    "url": "/React-for-Beginners/static/js/main.c2fb414a.chunk.js"
+  },
+  {
+    "revision": "1429910d2c37c79bef30",
+    "url": "/React-for-Beginners/static/js/1.1429910d.chunk.js"
+  },
+  {
+    "revision": "53c2f6395ccc58cdcbce",
+    "url": "/React-for-Beginners/static/js/runtime~main.53c2f639.js"
+  },
+  {
+    "revision": "538085a5416e8d47064c65a2a7e6add4",
+    "url": "/React-for-Beginners/index.html"
+  }
+];
